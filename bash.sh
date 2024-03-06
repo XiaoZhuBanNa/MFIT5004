028ff5bf777fa38c16ce6958fc64c71b069f2841
XiaoZhuBanNa <1114285985@qq.com>
2024-03-06 21:55:55 +0800

    docs: submit bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+mkdir DataMining
+cd DataMining/
+git clone https://github.com/XiaoZhuBanNa/MFIT5004.git
+pip install -U "huggingface_hub[cli]"
+pip install numpy, pandas, peft, transformers, datasets, logging
