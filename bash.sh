555de41a1a7bce3da8a38b850565228cf21438a9
XiaoZhuBanNa <1114285985@qq.com>
2024-03-07 17:10:16 +0800

    feat: split conda installation and python package installation

--- bash.sh
+++ bash.sh
@@ -1,16 +1,8 @@
-# create a directory to install minicaonda in
-mkdir -p ~/miniconda3
-# download latest miniconda version
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-# run the install script
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-# delete the intall script
-rm -rf ~/miniconda3/miniconda.sh
-# add a conda initialize to your bash
-~/miniconda3/bin/conda init bash
-# Verify the installaton 
-conda list
-
 # package
 pip install -U "huggingface_hub[cli]"
-pip install numpy, pandas, peft, transformers, datasets, logging
+pip install numpy
+pip install pandas
+pip install peft
+pip install transformers
+pip install datasets
+pip install logging
