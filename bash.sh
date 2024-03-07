f9f3ce30f8be061ba9e286031a2971f163e53e0b
XiaoZhuBanNa <1114285985@qq.com>
2024-03-07 17:06:38 +0800

    feat: add conda installation command in bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,16 @@
-mkdir DataMining
-cd DataMining/
-git clone https://github.com/XiaoZhuBanNa/MFIT5004.git
+# create a directory to install minicaonda in
+mkdir -p ~/miniconda3
+# download latest miniconda version
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+# run the install script
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+# delete the intall script
+rm -rf ~/miniconda3/miniconda.sh
+# add a conda initialize to your bash
+~/miniconda3/bin/conda init bash
+# Verify the installaton 
+conda list
+
+# package
 pip install -U "huggingface_hub[cli]"
 pip install numpy, pandas, peft, transformers, datasets, logging
